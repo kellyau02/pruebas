cd196a7c5ab298bbdcef487efb6ec73f33f8799d
kellyau02 <45693238+kellyau02@users.noreply.github.com>
2021-09-30 20:26:04 -0600

    Delete l10n_cr_edi_sale_coupon directory

--- l10n_cr_edi_sale_coupon/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
-from . import sale
-from . import res_config_settings
-from . import res_company
--- l10n_cr_edi_sale_coupon/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
-from . import test_sale_coupon
